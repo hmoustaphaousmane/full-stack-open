af1e8f809df949743afa2dc68996c9454b62b60a
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-10-15 13:22:46 +0100

    feat: submission for part6: better anecdotes step 7

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,9 +3,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state =>
-    state.sort((a, b) => b.votes - a.votes) // order by the number of votes
-  )
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    anecdotes = anecdotes.sort((a, b) => b.votes - a.votes) // order by the number of votes
+    if (filter === '')
+      return anecdotes
+    return anecdotes.filter(anecdote => anecdote.content.includes(filter))
+  })
   const dispatch = useDispatch()
 
   return (
---
+++ part6/redux-anecdotes/src/components/Filter.jsx
@@ -0,0 +1,24 @@
+import { useDispatch, useSelector } from "react-redux"
+import { filterChange } from "../reducers/filterReducer"
+
+const Filter = () => {
+  const dispatch = useDispatch()
+
+  const handleChange = (event) => {
+    // input-field value is in variable event.target.value
+    const filter = event.target.value
+    console.log(filter)
+    dispatch(filterChange(filter))
+  }
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
