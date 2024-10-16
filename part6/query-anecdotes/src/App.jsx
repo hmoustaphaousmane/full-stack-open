8deb129f287c0676c7c8a96f8ce7fc54bfc47166
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-10-16 16:46:22 +0100

    feat: submission for part6: query anecdotes
    - retrieve anecdotes from the server using react query
    - add new anecdotes using react query
    - implement voting for anecdotes using again react query

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,36 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes, voteAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const voteMutation = useMutation({
+    mutationFn: voteAnecdote,
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+  })
 
   const handleVote = (anecdote) => {
     console.log('vote')
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if (result.isPending)
+    return <div>Loading...</div>
+
+  if (result.isError)
+    return <div>anecdote service not available du to problem in server</div>
+
+  const anecdotes = result.data
 
   return (
     <div>
